d04fb9765ec043ac9cb2e4ed4b876cf0a29c8364
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2024-12-27 18:59:29 +0100

    Add project files.

---
+++ WinMain.cpp
@@ -0,0 +1,14 @@
+﻿#include "WinMain.h"
+
+INT WINAPI wWinMain(
+	_In_ [[maybe_unused]] HINSTANCE instance,
+	_In_opt_ [[maybe_unused]] HINSTANCE prev_instance,
+	_In_ [[maybe_unused]] PWSTR cmd_line,
+	_In_ [[maybe_unused]] INT cmd_show
+) {
+	MessageBox(
+		nullptr, TEXT("Cześć świecie!!!"),
+		TEXT("Informacja"), MB_ICONINFORMATION
+	);
+	return 0;
+}
